ba204564dd913bcb0faf98e054636e5801ef0d13
XueK66 <74094966+XueK66@users.noreply.github.com>
2024-08-12 09:51:36 -0700

    修复bug

--- CoolQAPI/CoolQAPI/post_server.py
+++ CoolQAPI/CoolQAPI/post_server.py
@@ -36,7 +36,7 @@ class PostServer(Thread):
 
     def run(self):
         self.__server.logger.info('CoolQAPI server is starting up')
-        self.__server.logger.info(f"Listening address: {self.__config['post_host']}:{self.__config['post_port']}/{self.__config["post_path"]}")
+        self.__server.logger.info(f"Listening address: {self.__config['post_host']}:{self.__config['post_port']}/{self.__config['post_path']}")
         self.__post_server.run(
             port=self.__config['post_port'],
             host=self.__config['post_host'],
